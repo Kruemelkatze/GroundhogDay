58632717ba6c560646ba1cd718bf8e824473f6aa
Fabian <schoberf@gmx.net>
2016-08-06 01:09:07 +0200

    Basic Structure,  some fundamental Logic Elements

---
+++ Groundhog Day/Assets/_Scripts/Game.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+
+	//Stores all (globally) relevant game variables
+
+	void Start() {
+		Grid.EventHub.TriggerExampleIntegerEvent(5);
+		Grid.EventHub.ExampleIntegerEvent += OnExampleIntegerEvent;
+		Grid.EventHub.TriggerExampleIntegerEvent(15);
+	}
+
+	void OnDestroy() {
+		/* Unregister Events */
+		Grid.EventHub.ExampleIntegerEvent -= OnExampleIntegerEvent;
+	}
+
+	public void OnExampleIntegerEvent(int value) {
+		Debug.Log ("ExampleIntegerEvent ... " + value);
+	}
+
+	void Update() {
+		/*if (Input.GetKeyDown (KeyCode.Escape)) {
+			SceneManager.LoadScene ("Title");
+		}*/
+	}
+
+
+}
